48c4a400175f3911992861df1c4246c7a3d195c9
KimUSIU <hongki990806@gmail.com>
2024-06-11 17:57:40 +0300

    Initial Commit

---
+++ OpenGLDemo/OpenGLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGLDemoApp.swift
+//  OpenGLDemo
+//
+//  Created by 김홍기 on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenGLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
